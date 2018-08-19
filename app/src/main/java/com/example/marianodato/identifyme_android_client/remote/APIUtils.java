c6ea75bccd80cdd326aef286be98c9be129347bf
marianodato <marianodato@gmail.com>
2018-08-18 21:59:35 -0300

    Project structure and retrofit

--- app/src/main/java/com/example/marianodato/identifyme_android_client/remote/APIUtils.java
+++ app/src/main/java/com/example/marianodato/identifyme_android_client/remote/APIUtils.java
@@ -3,7 +3,7 @@ package com.example.marianodato.identifyme_android_client.remote;
 public class APIUtils {
 
     private APIUtils(){
-    };
+    }
 
     public static final String API_URL = "http://localhost:8080/demo/";
 
